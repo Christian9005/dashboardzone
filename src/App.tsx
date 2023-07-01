00421d0d327c55951abf904bfa6c3167959567b1
Christian Albarracin <christianalex-071@hotmail.com>
2023-06-30 20:00:38 -0500

    updateApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import GoogleSheetsData from "./components/template/GoogleSheetsData/GoogleSheetsData";
-import CardPrincipal from "./components/molecules/CardPrincipal/CardPrincipal";
-
 
 function App() {
   return (
